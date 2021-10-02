7c90baa6bed45906ba3f2c4fba025d27868fcc80
Rishabh Singh <ris8645208@gmail.com>
2021-10-02 17:10:57 +0530

    feat: add redirects to routes and status codes

--- server.js
+++ server.js
@@ -10,12 +10,20 @@ const server = http.createServer((req, res) => {
   switch (req.url) {
     case "/":
       path += "index.html";
+      res.statusCode = 200;
       break;
     case "/about":
       path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-us":
+      res.setHeader("Location", "/about");
+      res.statusCode = 301;
+      res.end();
       break;
     default:
       path += "404.html";
+      res.statusCode = 404;
       break;
   }
 
