5c6b7e8cf384c6e269f4f2fcb644df9287547e7e
Rishabh Singh <ris8645208@gmail.com>
2021-10-02 15:45:22 +0530

    chore: add basic routing

--- server.js
+++ server.js
@@ -6,15 +6,35 @@ const server = http.createServer((req, res) => {
   console.log("request made", req.url, req.method);
   console.log(res);
   res.setHeader("Content-Type", "text/html");
-  fs.readFile("./views/index.html", { encoding: "utf-8" }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end();
-    } else {
-      //   res.write(data);
-      res.end(data);
-    }
-  });
+  switch (req.url) {
+    case "/":
+      fs.readFile("./views/index.html", { encoding: "utf-8" }, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.end();
+        } else {
+          res.end(data);
+        }
+      });
+    case "/about":
+      fs.readFile("./views/about.html", { encoding: "utf-8" }, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.end();
+        } else {
+          res.end(data);
+        }
+      });
+    default:
+      fs.readFile("./views/404.html", { encoding: "utf-8" }, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.end();
+        } else {
+          res.end(data);
+        }
+      });
+  }
 });
 
 server.listen(3000, "localhost", () => {
